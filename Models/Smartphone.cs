813a472b334e953b3c89c8012e1a23f040259902
anaximenesLima <anaximenes.lima@gmail.com>
2022-11-08 11:04:55 -0300

    Desafio POO Concluido git status!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,27 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
         }
 
-        public void Ligar()
+        public void Ligar(string contato)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando... para " + contato);
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string contato)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação... de " + contato);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Srmatphone Nokia: ");
+Nokia n = new Nokia(numero: "9999-9999", modelo: "N1234", imei: "1234", memoria: 64);
+n.Ligar("1123-1234");
+n.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Srmatphone Iphone: ");
+Iphone i = new Iphone(numero: "1111-1111",modelo: "I123",imei:  "123", memoria:  128);
+i.ReceberLigacao("9991-9992");
+i.InstalarAplicativo("Spotfy");
